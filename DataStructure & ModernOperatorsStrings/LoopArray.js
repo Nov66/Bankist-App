0b3229d3d1f19fd2ce228c0f2b761a164539e07b
Nov66 <liunov13@hotmail.com>
2021-08-19 14:43:37 +1000

    practice Object Literals and Optioanl Chaining

--- DataStructure & ModernOperatorsStrings/LoopArray.js
+++ DataStructure & ModernOperatorsStrings/LoopArray.js
@@ -1,4 +1,24 @@
 'use strict';
+// HIGHLIGHT: Object Literals
+const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  // NOTE: = [weekdays[3]]
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  // NOTE: = [weekdays[4]]
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  // NOTE: = [weekdays[5]] or can even write [`day-${2 + 4}`]
+  sat: {
+    open: 0,
+    close: 24, // 12 + 12
+  },
+};
+
 // HIGHLIGHT: Looping Arrays -> for-of loop
 const restaurant = {
   name: 'Classico Italiano',
@@ -6,24 +26,15 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprses Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0,
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  // openingHours: openingHours, NOTE: same as variable name
+
+  // NOTE: ES6 enhanced object literals
+  openingHours,
+  // NOTE: no need to write FUNCTION
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },

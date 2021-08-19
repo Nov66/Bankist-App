e4a41769ea347b0488cee2c4249183297167be60
Nov66 <liunov13@hotmail.com>
2021-08-19 23:26:05 +1000

    adjust format for set.js

--- DataStructure & ModernOperatorsStrings/sets.js
+++ DataStructure & ModernOperatorsStrings/sets.js
@@ -10,17 +10,21 @@ const orderSet = new Set([
 ]);
 console.log(orderSet); // NOTE: all the duplicates has gone: {"Pasta", "pizza", "risotto", "pasta"}
 console.log(new Set('Jonas')); //  {"J", "o", "n", "a", "s"}
+
 // HIGHLIGHT: Check length of the SET
 console.log(orderSet.size);
+
 // HIGHLIGHT: Check whether include element = array.include
 console.log(orderSet.has('pizza'));
 console.log(orderSet.has('Bread'));
+
 // HIGHLIGHT: CANNOT Retrieve data -> NO index -> CANNOT get data outside of SETS
 orderSet.add('Garlic Bread');
 orderSet.add('Garlic Bread');
 orderSet.delete('risotto');
 // orderSet.clear(); NOTE: clear the whole set
 console.log(orderSet);
+
 // HIGHLIGHT: LOOPING Sets
 for (const order of orderSet) {
   console.log(order);
@@ -28,9 +32,11 @@ for (const order of orderSet) {
 
 // TODO: Example
 const staff = ['waiter', 'chef', 'waiter', 'manager', 'chef', 'waiter'];
+
 // HIGHLIGHT: convert Set to Array
 const staffUnique = [...new Set(staff)];
 console.log(staffUnique);
+
 // HIGHLIGHT: To know the size of set without define them
 console.log(
   new Set(['waiter', 'chef', 'waiter', 'manager', 'chef', 'waiter']).size

befe56eff7575c81f901bef03b6d7568c2f5bf63
Nov66 <liunov13@hotmail.com>
2021-08-27 15:22:13 +1000

    feat(closeAccount): Add close account function by using splice and findIndex method

--- BankistApp/script.js
+++ BankistApp/script.js
@@ -213,3 +213,25 @@ btnTransfer.addEventListener('click', function (e) {
     updateUI(currentAccount);
   }
 });
+
+// HIGHLIGHT: Close Account by using Splice and FindIndex method
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  // console.log('Delete');
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // Delete Account
+    accounts.splice(index, 1); // NOTE: No need to store because it Mutate original array
+
+    // Hide UI
+    labelWelcome.textContent = 'Log in to get started';
+    containerApp.style.opacity = 0;
+  }
+});

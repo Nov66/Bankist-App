38df4fc3dbca2362ea940d20f15ab04bd3fd8acd
Nov66 <liunov13@hotmail.com>
2021-09-09 17:15:01 +1000

    practice ES6 classes to implement OOP in JS

---
+++ OOPwithJS/ES6classes.js
@@ -0,0 +1,44 @@
+'use strict';
+/* HIGHLIGHT: ES6 classes
+- Classes are still Functions
+- Constructor is automatically called when we create a new object
+using NEW operator
+- Classes are NOT Hoisted → for both Expression and Declaration
+- Classes are also first-class citizens → Means we can pass them into functions, and also return them from functions → because classes are really a special kind of function behind the scenes
+- Classes are executed in Strict mode
+ */
+/* NOTE: Class Expression
+const PersonCl = class {};
+*/
+// NOTE: Class Declaration
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  /* NOTE:
+  - Methods write outside the Constructor will be on the Prototype of the Objects
+  - NOT on the objects themselves 
+  - Methods will be added to .prototype property
+  */
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`HEY ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge(); // 41
+console.log(jessica.__proto__ === PersonCl.prototype); // true
+
+/*
+PersonCl.prototype.greet = function () {
+  console.log(`HEY ${this.firstName}`);
+};
+*/
+jessica.greet();
